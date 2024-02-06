922ae3e6242f76bce8a86d748ecf3fd81447ca98
druz3 <oliver@tangen.biz>
2024-02-06 19:29:21 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <html lang="en">
+      <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>NTNU-reviews</title>
+          <link rel="stylesheet" href="style.css" />
+      </head>
+      <body>
+          <header>
+              <nav>
+                  <div id="logo">NTNU Reviews</div>
+
+                  <div class="box">Info</div>
+              </nav>
+          </header>
+
+          <main>
+              <div id="search-container">
+                  <input
+                      class="box"
+                      id="search"
+                      type="text"
+                      placeholder="Search Subject: "
+                  />
+              </div>
+          </main>
+
+          <footer></footer>
+          <script src="script.js"></script>
+      </body>
+  </html>
   );
 }
 
